e7518ad30bc89b3bed1ca463357a7402ee649432
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-07-01 06:10:43 +0530

    few designs changed

--- src/services/GetStartedPage.jsx
+++ src/services/GetStartedPage.jsx
@@ -651,7 +651,7 @@ const GetStartedPage = () => {
             {/* Header */}
             <Header />
 
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 mt-12">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                     {/* Left Side - Benefits */}
                     <div className="lg:col-span-4">
