6f1a790be7bca966453aa52eca39f6562ce21eed
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-08-22 16:38:32 +0530

    checkout user info connect with database

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -180,7 +180,7 @@ const Checkout = () => {
         <div className="min-h-screen bg-gray-50">
             <Header />
 
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-14">
                 {/* Header */}
                 <div className="mb-8">
                     <Link
